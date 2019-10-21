50163a90b42d064d20f2d2ed1019efff919d3837
allenyin <yinxinla@163.com>
2019-10-21 22:57:45 +0800

    done matching element

--- src/main/java/com/airta/action/engine/message/report/ReportSubscriber.java
+++ src/main/java/com/airta/action/engine/message/report/ReportSubscriber.java
@@ -27,35 +27,65 @@ public class ReportSubscriber {
         logger.info("listen report topic: " + record.key());
         logger.info("listen report topic value: " + record.value().toString());
 
-        if(record.value()!=null) {
+        if (record.value() != null) {
             Object rootElementObject = jsonParser.resolveIncomingMessage(record.value().toString(), Element.class);
-            if(rootElementObject!=null) {
-                Element rootElement  = (Element)rootElementObject;
+            if (rootElementObject != null) {
+                Element rootElement = (Element) rootElementObject;
                 logger.info("## now we resolved report element: {}", rootElement);
-                List elementList = new ArrayList(){};
-                elementList.add(rootElement);
-                jsonParser.elementToJsonFile(elementList);
+                jsonParser.updateExistingElementWithNewMessage(rootElement);
             }
         }
-
     }
 
     public static void main(String[] args) {
 
         ReportSubscriber reportSubscriber = new ReportSubscriber();
+//        List<Element> elementList = reportSubscriber.initMap();
+//        reportSubscriber.writeToFile(elementList);
+
+        Element incomingElement = reportSubscriber.updateMap();
+        reportSubscriber.updateExs(incomingElement);
+    }
+
+    private void updateExs(Element incomingElement) {
+        jsonParser.updateExistingElementWithNewMessage(incomingElement);
+    }
+
+    public List<Element> initMap() {
+
         List<Element> rootList = new ArrayList<>();
         Element element = new Element();
         element.setText("single_01");
-        element.setElementId("single_01");
+        element.setElementId("single_011");
         List<Element> children = new ArrayList<>();
         Element element_01 = new Element();
-        element_01.setText("single_01");
+        element_01.setText("single_012");
+        element_01.setElementId("single_012");
         Element element_02 = new Element();
-        element_02.setText("single_02");
+        element_02.setText("single_013");
+        element_02.setElementId("single_013");
         Element element_03 = new Element();
-        element_03.setText("single_03");
+        element_03.setText("single_014");
+        element_03.setElementId("single_014");
         Element element_04 = new Element();
-        element_04.setText("single_04");
+        element_04.setText("single_015");
+        element_04.setElementId("single_015");
+
+        Element element_021 = new Element();
+        element_021.setText("single_0212");
+        element_021.setElementId("single_0212");
+        Element element_022 = new Element();
+        element_022.setText("single_0213");
+        element_022.setElementId("single_0213");
+        Element element_023 = new Element();
+        element_023.setText("single_0214");
+        element_023.setElementId("single_0214");
+        List<Element> ele2children = new ArrayList<>();
+        ele2children.add(element_021);
+        ele2children.add(element_022);
+        ele2children.add(element_023);
+        element_02.setChildren(ele2children);
+
         children.add(element_01);
         children.add(element_02);
         children.add(element_03);
@@ -63,12 +93,35 @@ public class ReportSubscriber {
         element.setChildren(children);
         rootList.add(element);
 
-        reportSubscriber.writeToFile(rootList);
+        return rootList;
+    }
+
+    private Element updateMap() {
+
+        Element element_02 = new Element();
+        element_02.setText("single_014");
+        element_02.setElementId("single_014");
+
+        Element element_021 = new Element();
+        element_021.setText("single_0312");
+        element_021.setElementId("single_0312");
+        Element element_022 = new Element();
+        element_022.setText("single_0313");
+        element_022.setElementId("single_0313");
+        Element element_023 = new Element();
+        element_023.setText("single_0314");
+        element_023.setElementId("single_0314");
+        List<Element> ele2children = new ArrayList<>();
+        ele2children.add(element_021);
+        ele2children.add(element_022);
+        ele2children.add(element_023);
+        element_02.setChildren(ele2children);
+
+        return element_02;
     }
 
     private void writeToFile(List<Element> rootList) {
-//        jsonParser.elementToJsonFile(rootList);
-        jsonParser.updateToJsonFile();
+        jsonParser.elementToJsonFile(rootList);
     }
 
 
--- src/main/java/com/airta/action/engine/parser/JsonParser.java
+++ src/main/java/com/airta/action/engine/parser/JsonParser.java
@@ -2,6 +2,7 @@ package com.airta.action.engine.parser;
 
 import com.airta.action.engine.entity.report.Element;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import org.slf4j.Logger;
@@ -14,6 +15,7 @@ import org.springframework.util.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -44,7 +46,7 @@ public class JsonParser {
         } catch (JsonProcessingException e) {
             logger.error(e.getMessage());
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(e.getMessage());
         }
 
         /**
@@ -118,32 +120,115 @@ public class JsonParser {
         return StringUtils.trimAllWhitespace(input);
     }
 
-    public void elementToJsonFile(List rootElement) {
+    public void updateExistingElementWithNewMessage(Element incomingRootElement) {
 
-        File report = new File(TreeJsonPath);
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            mapper.writeValue(report, rootElement);
-        } catch (IOException e) {
-            e.printStackTrace();
+        File oldSitemapFile = new File(TreeJsonPath);
+        if (!oldSitemapFile.exists()) {
+            logger.info("## Previous sitemap JSON file not exists, initiate it.");
+            List elementList = new ArrayList();
+            elementList.add(incomingRootElement);
+            elementToJsonFile(elementList, oldSitemapFile);
+        } else {
+            logger.info(" Updating previous sitemap ..");
+            findAndUpdateTobeUpdatedRootElementFromOldSitemap(oldSitemapFile, incomingRootElement);
+
         }
     }
 
-    public void updateToJsonFile() {
+    private Element findAndUpdateTobeUpdatedRootElementFromOldSitemap(File oldSiteMapFile, Element incomingRootElement) {
 
-        File report = new File(TreeJsonPath);
+        logger.info("Locating matching element by incoming element from previous sitemap");
         ObjectMapper mapper = new ObjectMapper();
         try {
-            List obj = mapper.readValue(report, List.class);
-            if(obj!=null) {
-                List<Element> elementList = (List<Element>)obj;
-                logger.info("## Element list children size: {}", elementList.size());
-                List<Element> childrenList = elementList.get(0).getChildren();
-
+            List obj = mapper.readValue(oldSiteMapFile, new TypeReference<List<Element>>(){});
+            if (obj != null) {
+                List<Element> oldElementList = (List<Element>) obj;
+                logger.info("## Element list children size: {}", oldElementList.size());
+                if (oldElementList.size() > 0) {
+                    Element oldRootElement = oldElementList.get(0);
+                    Element matchingElement = findFromSpecifiedOldElement(oldRootElement, incomingRootElement);
+                    if (matchingElement != null) {
+                        logger.info("## Found match element: {} from existing sitemap.", matchingElement.getElementId());
+                        replaceElementWithIncoming(matchingElement, incomingRootElement);
+                        elementToJsonFile(oldElementList, oldSiteMapFile);
+                    } else {
+                        logger.warn("## Cannot locate matching element ..");
+                    }
+                }
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error(e.getLocalizedMessage());
         }
-
+        return null;
     }
+
+    private Element findFromSpecifiedOldElement(Element oldStartElement, Element incomingRootElement) {
+
+        if (compareElementsIfMatching(oldStartElement, incomingRootElement)) {
+            logger.info("## start element matching, update the start one.");
+            return oldStartElement;
+        } else {
+            List<Element> oldChildrenList = oldStartElement.getChildren();
+            if(oldChildrenList.isEmpty()) {
+                return null;
+            }
+            for (Element oldChildElement : oldChildrenList) {
+                Element matchingElement = findFromSpecifiedOldElement(oldChildElement, incomingRootElement);
+                if(matchingElement==null) {
+                    continue;
+                } else {
+                    return matchingElement;
+                }
+            }
+        }
+        return null;
+    }
+
+    private boolean compareElementsIfMatching(Element oldElement, Element incomingElement) {
+
+        if (oldElement.getElementId() != null && incomingElement.getElementId() != null) {
+            return oldElement.getElementId().equals(incomingElement.getElementId());
+        } else if (oldElement.getPathPath() != null && incomingElement.getPathPath() != null) {
+            return oldElement.getPathPath().equals(incomingElement.getPathPath());
+        } else {
+            logger.warn("Element id or path NULL, compare fail.");
+            return false;
+        }
+    }
+
+    private void replaceElementWithIncoming(Element oldElement, Element newElement) {
+
+        oldElement.setChildren(newElement.getChildren());
+        oldElement.setChildrenCount(newElement.getChildrenCount());
+        oldElement.setText(newElement.getText());
+        oldElement.setElementId(newElement.getElementId());
+        oldElement.setActionable(newElement.isActionable());
+        oldElement.setWorkingOn(newElement.isWorkingOn());
+        oldElement.setPathPath(newElement.getPathPath());
+        oldElement.setParentId(newElement.getParentId());
+        oldElement.setUrl(newElement.getUrl());
+        oldElement.setType(newElement.getType());
+        oldElement.setId(newElement.getId());
+    }
+
+    /**
+     * export incoming element to JSON file as sitemap.
+     *
+     * @param rootElement
+     */
+    public void elementToJsonFile(List rootElement, File siteMapFile) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            mapper.writeValue(siteMapFile, rootElement);
+        } catch (IOException e) {
+            logger.error(e.getLocalizedMessage());
+        }
+    }
+
+    public void elementToJsonFile(List rootElement) {
+
+        elementToJsonFile(rootElement, new File(TreeJsonPath));
+    }
+
 }
