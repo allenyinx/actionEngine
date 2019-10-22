78ee3f8f27b87eac3ade0a6baaff91fe76a1761f
I306445 <allen.yin@sap.com>
2019-10-22 14:22:00 +0800

    add container into deployment yaml

--- sitemap/build.sh
+++ sitemap/build.sh
@@ -6,9 +6,6 @@ Release=1.0
 timestamp=$(date +"%Y%m%d%H.%s")
 ImageGlobalTag=${Release}-${timestamp}
 
-./mvnw clean
-./mvnw package -DskipTests
-
 docker build -t ${DockerCategory}/${NAME}:${ImageGlobalTag} -f Dockerfile . || exit 1
 docker tag ${DockerCategory}/${NAME}:${ImageGlobalTag} ${DockerCategory}/${NAME}:latest
 docker push ${DockerCategory}/${NAME}:${ImageGlobalTag}
